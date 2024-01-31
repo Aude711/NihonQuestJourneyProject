5966efb2caafac5ae7eb3c3a127391eb08f18130
Aude Monnet <aude.monnet711@gmail.com>
2024-01-31 17:56:17 +0100

    finish the back part for the view region page but didnt made the css yet

--- src/Controller/RegionController.php
+++ src/Controller/RegionController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\City;
+use App\Entity\Recommendation;
+use App\Entity\Region;
 use App\Repository\CityRepository;
+use App\Repository\RecommendationRepository;
 use App\Repository\RegionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/region', name: 'region_')]
 class RegionController extends AbstractController
 {
-    #[Route('/region', name: 'app_region')]
+    #[Route('/', name: 'index')]
     public function index(RegionRepository $regionRepository, CityRepository $cityRepository): Response
     {
         $region = $regionRepository->findAll();
@@ -18,6 +23,21 @@ class RegionController extends AbstractController
         return $this->render('region/index.html.twig', [
             'region' => $region,
             'city' => $city,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'view', methods: ['GET', 'POST'])]
+    public function show(
+        Region $region,
+        CityRepository $cityRepository,
+        RecommendationRepository $adviceRepository
+    ): Response {
+        $city = $cityRepository->findBy(['region' => $region]);
+        $recommendation = $adviceRepository->findBy(['city' => $city]);
+        return $this->render('region/view.html.twig', [
+            'region' => $region,
+            'city' => $city,
+            'recommendation' => $recommendation,
         ]);
     }
 }
